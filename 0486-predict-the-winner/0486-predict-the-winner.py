48833e66e2c1a51a8fe161d32aafdbc2e59fe1d5
JASHASWI <jashaswi04@gmail.com>
2026-08-02 18:53:48 +0530

    Time: 408 ms (22.66%), Space: 19.3 MB (79.82%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,8 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n=len(nums)
+        if ~n & 1: return True
+        def maxDiff(i:int,j:int)->int:
+            if i==j: return nums[i]
+            return max(nums[i]- maxDiff(i+1,j),nums[j]-maxDiff(i,j-1))
+        return maxDiff(0,n-1)>=0
